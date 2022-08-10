8e09d49bcc046e6acc634e9ac52b3ea6422365f7
unknown <islam15-3879@diu.edu.bd>
2022-08-10 16:45:46 +0600

    card-design

--- src/components/SingleFood/SingleFood.js
+++ src/components/SingleFood/SingleFood.js
@@ -4,17 +4,18 @@ const SingleFood = ({food}) => {
     const {  name , price , description,quantity , supliar ,img}=food;
     return (
         <div class="card  bg-base-100 shadow-xl">
-  <figure><img src={img} alt="Shoes" /></figure>
+  <figure><img className='w-auto rounded-lg h-64' src={img} alt="Shoes" /></figure>
   <div class="card-body">
     <h2 class="card-title">
       {name}
-      <div class="badge badge-primary">{price}taka</div>
+      <div class="badge badge-primary">{price}</div>
     </h2>
     <p>{description}</p>
     <div class="card-actions justify-end">
-      <div class="badge badge-outline">{supliar}</div> 
+      <div class="badge badge-outline">Quantity</div> 
       <div class="badge badge-outline">{quantity}</div>
     </div>
+    <button class="btn btn-secondary font-bold w-full ">Details</button>
   </div>
 </div>
     );
